2c85efaef09dcf5fe93f879dbc6c4537fc642e91
Jacastro619 <jorgecastro619@gmail.com>
2023-10-17 15:11:15 -0700

    first commit with starter files

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  // use inquire to display questions from the quesitons array for user
+  // call generate markdown on gathered data
+  // take what was returned from generatemarkdown and pass it to writetofile function
+}
+
+// Function call to initialize app
+init();
+
+//
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,24 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+// create a conditional to see if license exist
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+// create a conditional to see if license exist
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+// create a conditional to see if license exist
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  // take answers and plug it in to the template literal as a mark down structure
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
