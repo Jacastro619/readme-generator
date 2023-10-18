25b175d3e80254911b6122b9625c7a096c17e8fa
Jacastro619 <jorgecastro619@gmail.com>
2023-10-18 15:04:22 -0700

    added psuedocode and imported needed moduels

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+// add questions into array with type, name, and message
+// add readme sections in questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// use FS module to write data to readme file, filename
 
 // TODO: Create a function to initialize app
 function init() {
@@ -15,5 +21,3 @@ function init() {
 
 // Function call to initialize app
 init();
-
-//
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,15 +7,19 @@ function renderLicenseBadge(license) {}
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 // create a conditional to see if license exist
+// return the physicall link of the license
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 // create a conditional to see if license exist
+// call renderlicenseBage and renderLicenseBage functions
+// return template literal to format license section
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   // take answers and plug it in to the template literal as a mark down structure
+  // you may call appropiate fuinctions in template literal
   return `# ${data.title}
 
 `;
